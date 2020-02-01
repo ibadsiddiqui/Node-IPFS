c9aba1977b6f218f7d8d01090cad69daf76b037e
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-02-01 23:03:21 +0500

    --Commit: peerID added

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ let bcrypt = require('bcryptjs')
 // const UpdatePassword = require('./../api/users/update_password')
 const UserLogin = require('./../api/users/login')
 // const IPFS = require("ipfs");
-// const peerID = require('peer-id');
+const PeerId = require('peer-id');
 
 module.exports = function (mongoose) {
   const modelName = 'user'
@@ -31,7 +31,6 @@ module.exports = function (mongoose) {
       field: '_role',
     }
   }, { strict: false })
-  const SchemaForPopulation = Schema;
   Schema.statics = {
     collectionName: modelName,
     routeOptions: {
@@ -43,74 +42,26 @@ module.exports = function (mongoose) {
       },
       extraEndpoints: [
         UserLogin,
-        // UpdatePassword,
-        // Password Update Endpoint
-        // function (server, model, options, logger) {
-        //   server.route({
-        //     method: 'PUT',
-        //     path: '/user/{_id}/password',
-        //     config: {
-        //       handler: (req, h) => updatePasswordHandler(req, h, model, logger),
-        //       auth: null,
-        //       description: "Update a user's password.",
-        //       tags: ['api', 'User', 'Password'],
-        //       validate: {
-        //         params: {
-        //           _id: RestHapi.joiHelper.joiObjectId().required()
-        //         },
-        //         payload: {
-        //           password: Joi.string()
-        //             .required()
-        //             .description("The user's new password")
-        //         }
-        //       },
-        //       plugins: {
-        //         'hapi-swagger': {
-        //           responseMessages: [
-        //             { code: 200, message: 'Success' },
-        //             { code: 400, message: 'Bad Request' },
-        //             { code: 404, message: 'Not Found' },
-        //             { code: 500, message: 'Internal Server Error' }
-        //           ]
-        //         }
-        //       }
-        //     }
-        //   })
-        // }
+        UpdatePassword,
       ],
       create: {
         pre: async function (payload, logger) {
 
           const id = await PeerId.create({ bits: 1024, keyType: 'rsa' })
-
+          const detailsOfId = await id.toJSON();
           const hashedPassword = mongoose
             .model('user').generatePasswordHash(payload.password);
 
           payload.password = hashedPassword;
-          payload.peerID = id;
+
+          payload.peerID = Object({
+            id: detailsOfId.id,
+            privKey: detailsOfId.privKey,
+            pubKey: detailsOfId.pubKey
+          });
 
           return payload
         },
-        // post: async function (data) {
-        //   console.log(data);
-
-        //   const User = mongoose.model("user", SchemaForPopulation);
-        //   await User.findByID(data._id.toString(), async (err, user) => {
-        //     console.log(err);
-
-        //     var opts = [{ path: '_role' }];
-
-        //     await User.populate(user, opts, function (err, user) {
-        //       console.log(user);
-        //       return;
-        //     });
-
-        //   });
-        // const Users = await User.find({});
-        // console.log(Users);
-        // return;
-        // .populate("role").exec();
-        // }
       },
     },
 
