77bdd8f4a67e92204602d3469705cf7e9624180c
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-02-02 23:48:57 +0500

    --Commit: token and handler updated

--- handlers/user/login.handler.js
+++ handlers/user/login.handler.js
@@ -1,18 +1,17 @@
 
 const Boom = require('@hapi/boom')
 
-module.exports = async function (request, h, model, logger) {
+module.exports = async function (request, server, model, logger) {
     const Log = logger.bind('Password Update')
     const collectionName = model.collectionDisplayName || model.modelName
 
-    const User = model("user");
 
     Log.note('Generating Password Update endpoint for ' + collectionName)
     try {
         let token = "";
         let response = {};
 
-        let user = await User.findByCredentials(
+        let user = await model.findByCredentials(
             request.payload.email,
             request.payload.password,
             Log
@@ -27,8 +26,8 @@ module.exports = async function (request, h, model, logger) {
 
         response = { user, token };
 
-        // return response;
-        return h.response(response).code(200)
+        return response;
+        // return h.response(response).code(200)
     } catch (err) {
         Log.error(err)
         throw Boom.badImplementation(err)
